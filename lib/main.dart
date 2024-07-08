320c327c3e53b3eccd4cbc87e6790c693c91f8b6
Naman8585 <naman.nagigeek@gmail.com>
2024-07-08 18:05:37 +0530

    Weather App final

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_app/screens/home.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
@@ -11,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
